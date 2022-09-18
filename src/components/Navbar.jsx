eab4d614e0aba2bd89a9ffdf56e5b1037d951628
catalinfl <catalinfl02@gmail.com>
2022-09-18 23:48:37 +0300

    reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,26 @@ import { Button } from '@material-tailwind/react'
 import image from './navbar.png'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
+import { useNavigate } from 'react-router'
 
 const Navbar = () => {
+
+const navigate = useNavigate();
+
+
+const signOutFunction = () => {
+  signOut(auth);
+  navigate('/login');
+}
+
+
   return (
     <div className="navbar h-16 bg-red-700">
         <div className="user my-auto">
             <img src={image} alt="blabla" />
             <span className="text-sm my-auto"> name </span>
         </div>
-        <Button onClick={() => signOut(auth)} className="chatButton bg-red-400 shadow-sm w-16 h-5 justify-center flex items-center shadow-red-300 hover:shadow-red-300 rounded-none "> Log out</Button>
+        <Button onClick={signOutFunction} className="chatButton bg-red-400 shadow-sm w-16 h-5 justify-center flex items-center shadow-red-300 hover:shadow-red-300 rounded-none "> Log out</Button>
     </div>
   )
 }
