65db264c1d3c8d57ee5a9631975101fde2cb506b
catalinfl <catalinfl02@gmail.com>
2022-09-25 00:53:19 +0300

    scroll added

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -5,43 +5,64 @@ import { ChatContext } from '../context/ChatContext';
 import { db } from "../firebase";
 import { useRef } from 'react'
 import { AiOutlineDownCircle } from "react-icons/ai"
+import { AiOutlineUpCircle } from 'react-icons/ai';
 
 
 const Messages = () => {
 
   const [messages, setMessages] = useState([]);
   const { data } = useContext(ChatContext);
-  const [button, setButton] = useState(false);
+  const [buttonDown, setButtonDown] = useState(true);
+  const [buttonUp, setButtonUp] = useState(true);
 
   const ref = useRef();
+  
+  useEffect(() => {
+    setButtonDown(true);
+    setButtonUp(false);
+  }, [messages]) 
 
-  // useEffect(() => {
-  //   console.log(ref.current.scrollTop);
-  // }, [messages]) 
+
+
+
+  // if (messages) {
+  //   console.log(ref.current?.scrollHeight - 508)
+  //   console.log(ref.current?.scrollTop)
+  // }
 
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages);
+      doc.exists() && setMessages(doc.data().messages)
+      ;
     })
     return () => {
       unSub();
+      setButtonDown(true)
     }
   }, [data.chatId]);
 
-  const showDownButton = () => {
-    ref.current?.scroll({behavior: "smooth"})
-    ref.current?.scrollTo(0, ref.current.scrollHeight)
-    setButton(false);
+
+
+  const scrollDown = () => {
+    ref.current.scrollTo(0, ref.current?.scrollHeight);
+    setButtonDown(false);
+    setButtonUp(true);
   }
 
+  const scrollUp = () => {
+    ref.current.scrollTo(0, 0);
+    setButtonUp(false);
+    setButtonDown(true);
+  }
 
   return (
-    <div ref={ref} className="messages">
+    <div ref={ref} onDoubleClick={scrollDown} className="messages">
       {messages.map((message) => (
         <Message message={message} key={message.id} />
       ))}
-      <AiOutlineDownCircle onClick={showDownButton} className="downIcon"/>
+     {buttonDown && <AiOutlineDownCircle onClick={scrollDown} className="downIcon"/>}
+     {buttonUp && <AiOutlineUpCircle onClick={scrollUp} className="upIcon"/>}
     </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const signOutFunction = () => {
   return (
     <div className="navbar h-16 bg-red-700">
         <div className="user my-auto">
-            <img src={currentUser.photoURL} alt="blabla" />
+            <img src={currentUser.photoURL} alt="profilePhoto" />
             <span className="text-sm my-auto"> {currentUser.displayName} </span>
         </div>
         <Button onClick={signOutFunction} className="chatButton bg-red-400 shadow-sm w-16 h-5 justify-center flex items-center shadow-red-300 hover:shadow-red-300 rounded-none "> Log out </Button>
