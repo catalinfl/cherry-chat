5633e1b9e58c6a65ed6874892c77e4ca977a0320
catalinfl <catalinfl02@gmail.com>
2022-09-25 15:25:32 +0300

    -

--- src/components/InputChat.jsx
+++ src/components/InputChat.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { FcStackOfPhotos } from "react-icons/fc";
@@ -10,6 +10,8 @@ import {
   serverTimestamp,
   Timestamp,
   updateDoc,
+  onSnapshot,
+  getDoc
 } from "firebase/firestore";
 import { db, storage } from "../firebase";
 import { v4 as uuid } from "uuid";
@@ -20,6 +22,7 @@ const InputChat = () => {
   const [img, setImg] = useState(null);
   const [valueLength, setValueLength] = useState(false);
   const [isEmptyInput, setIsEmptyInput] = useState(false);
+  const [isMinimumInput, setIsMinimumInput] = useState(false);
   const [anotherCharacters, setAnotherCharacters] = useState(0);
   const [anotherCharactersBool, setAnotherCharactersBool] = useState(false);
 
@@ -53,6 +56,9 @@ const InputChat = () => {
       );
     } else {
       if (text.length === 0) { setIsEmptyInput(true); return }
+      if ((text.length < 5) && (text.length > 0)) {
+        return setIsMinimumInput(true);
+      }     
       await updateDoc(doc(db, "chats", data.chatId), {
         messages: arrayUnion({
           id: uuid(),
@@ -61,27 +67,32 @@ const InputChat = () => {
           date: Timestamp.now(),
         }),
       });
-    }
+    
 
     await updateDoc(doc(db, "userChats", currentUser.uid), {
       [data.chatId + ".lastMessage"]: {
         text,
       },
       [data.chatId + ".date"]: serverTimestamp(),
-    });
+          });
 
     await updateDoc(doc(db, "userChats", data.user.uid), {
       [data.chatId + ".lastMessage"]: {
         text,
       },
       [data.chatId + ".date"]: serverTimestamp(),
-    });
+    },
+    );
+    
     setText("")
     setImg(null);
     setValueLength(false);
     setAnotherCharactersBool(false);
     setIsEmptyInput(false);
+    setIsMinimumInput(false);
   };
+}
+
 
   var numberOfCharactersAllowed = 151;
 
@@ -92,7 +103,6 @@ const InputChat = () => {
     setValueLength(false);
     setIsEmptyInput(false);
   }
-
     setAnotherCharactersBool(false);
     setIsEmptyInput(false);
 
@@ -110,8 +120,9 @@ const InputChat = () => {
   }
 
   const handleKey = (e) => {
-    e.code === "Enter" && handleSend() 
+    e.code === "Enter" && handleSend()  
   }
+
 
 
 
@@ -119,7 +130,7 @@ const InputChat = () => {
     <> 
     <div className="inputChat">
       <div className="inputChatContainer">
-        <Input onKeyDown={handleKey} varriant="outlined" type="text" onChange={(e) => onChangeText(e)} value={text} className="inputChatItem shadow-lg" label="chat" spellCheck="false" color="orange"/>
+        <Input onKeyUp={handleKey} varriant="outlined" type="text" onChange={(e) => onChangeText(e)} value={text} className="inputChatItem shadow-lg" label="chat" spellCheck="false" color="orange"/>
       </div>
         <input type="file" style={{display: 'none'}} id="file" onChange={(e) => onChangeText(e)} />
       <label htmlFor='file'> 
@@ -131,6 +142,7 @@ const InputChat = () => {
       { anotherCharactersBool && <div className="errorRemainingCharacters"> <p> {` ${anotherCharacters - 1} characters remaining... `} </p> </div>}
       { valueLength && <div className="errorMaxCharacters">  <FiAlertCircle className="errorIcon"/>  <p>  {`Maximum ${numberOfCharactersAllowed - 1} characters`} </p> </div> }
       { isEmptyInput && <div className="errorMaxCharacters">  <FiAlertCircle className="errorIcon"/>  <p> Write something before sending </p> </div> }
+      { isMinimumInput && <div className="errorMaxCharacters">  <FiAlertCircle className="errorIcon"/>  <p> Minimum 5 characters </p> </div> }
       </div>
       </>
 )
